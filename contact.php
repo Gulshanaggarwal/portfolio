77d6c48128974109ce167e66c55b0d4c92f05b61
Gulshan aggarwal <58553401+Gulshanaggarwal@users.noreply.github.com>
2021-01-01 15:29:49 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,12 +4,12 @@ if($_POST["submit"]){
 	$sendermail = $_POST['Email'];
     $phone = $_POST['phone'];
     $Message = $_POST['message'];
-    $to="gulshanaggarwal333@gmail.com";
+    $to="gulshanaggarwal333gmail.com";
     $subject='My website message';
 
     //mail body
 
-    $mailbody=" Name: $name\n Email: $sender_mail\n Phone_number: $phone\n\n$Message";
+    $mailbody=" Name: $name\n Email: $sender_mail\n Phone_number: $phone\n\n $Message";
 
     //mail function
     mail($to, $subject, $mailbody,"From: $name <$sendermail>");
